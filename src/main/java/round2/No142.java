5409978ff20e8e90560a1507b2e35cf70a92dde5
xunruibo <xunruibo@autohome.com.cn>
2018-10-21 23:02:29 +0800

    1

---
+++ src/main/java/round2/No142.java
@@ -0,0 +1,39 @@
+package round2;
+
+import com.viki.leetcode.firstLevel21.ListNode;
+
+/**
+ * @Author: xunruibo
+ * @Description:
+ * @Date: Created in 22:51 2018/10/21
+ * @Modified By:
+ */
+public class No142 {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean isCycle = false;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                isCycle = true;
+                break;
+            }
+        }
+        if (isCycle == false) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}
