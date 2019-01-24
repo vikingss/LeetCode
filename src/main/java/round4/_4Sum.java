eeb3f34c3948ba33727e7f8e774d6a4d4347dbd7
xunruibo <xunruibo@autohome.com.cn>
2019-01-24 14:22:16 +0800

    round4

--- src/main/java/round4/_4Sum.java
+++ src/main/java/round4/_4Sum.java
@@ -11,18 +11,18 @@ import java.util.List;
  * @Modified By:
  */
 public class _4Sum {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> resList = new ArrayList<>();
         Arrays.sort(nums);
         if(nums.length < 4) return resList;
         for(int i = 0; i < nums.length - 3; ++i){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1; j < nums.length - 2; ++j){
-                if(nums[j] == nums[j - 1]) continue;
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue; // j > i + 1 保证不是第一次比较才能跳过
                 int l = j + 1, h = nums.length - 1, find = target - nums[i] - nums[j];
                 while(l < h){
                     if(nums[l] + nums[h] == find) {
-                        resList.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[l],nums[h])));
+                        resList.add(Arrays.asList(nums[i],nums[j],nums[l],nums[h]));
                         l++;
                         h--;
                         while(l < h && nums[l] == nums[l - 1]) l++; //如果l和之前一样，要么重复计算，要么不是正确答案，所以++
@@ -34,5 +34,11 @@ public class _4Sum {
             }
         }
         return resList;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,0,0};
+        int target = 0;
+        fourSum(nums,target);
     }
 }
